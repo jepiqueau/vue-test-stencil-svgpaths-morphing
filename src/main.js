647769f2c3bf896213655a2c853ab0521cc2193a
Quéau Jean Pierre <queaujeanpierre@MacBook-Pro-de-Queau-Jean-Pierre.local>
2019-03-30 15:35:52 +0100

    fix warning messages

--- src/main.js
+++ src/main.js
@@ -5,15 +5,17 @@ import store from "./store";
 import { defineCustomElements as svgmorphing } from "stencil-svgpaths-morphing/dist/loader";
 import Ionic from "@ionic/vue";
 
-Vue.use(Ionic);
 
 Vue.config.ignoredElements = [/jeep-\w*/];
+Vue.config.ignoredElements = [/ion-\w*/];
 
+Vue.use(Ionic);
 svgmorphing(window);
 
+Vue.config.productionTip = false;
+
 new Vue({
-  el: "#app",
   router,
   store,
   render: h => h(App)
-});
+}).$mount("#app");
