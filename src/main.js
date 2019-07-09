7a796371584a9486fec63d9375eddd1a24103e23
jepiqueau <jepi.queau@free.fr>
2019-07-09 08:58:34 +0200

    update to @ionic/vue@0.0.9

--- src/main.js
+++ src/main.js
@@ -5,22 +5,22 @@ import store from "./store";
 import { defineCustomElements as svgmorphing } from "stencil-svgpaths-morphing/dist/loader";
 import "@ionic/core/css/ionic.bundle.css";
 
-//import Ionic from "@ionic/vue"; // commented to use the workaround
+import Ionic from "@ionic/vue"; // commented to use the workaround
 /* added as a workaround */
-import { defineCustomElements as ionic } from "@ionic/core/loader";
-import { addIcons } from "ionicons";
-import { ICON_PATHS } from "ionicons/icons";
+//import { defineCustomElements as ionic } from "@ionic/core/loader";
+//import { addIcons } from "ionicons";
+//import { ICON_PATHS } from "ionicons/icons";
 /* --------------------- */
 
-//Vue.config.ignoredElements = [/jeep-\w*/]; // commented to use the workaround
+Vue.config.ignoredElements = [/jeep-\w*/]; // commented to use the workaround
 /* added as a workaround */
-Vue.config.ignoredElements = [/^ion-/, /^jeep-/];
+//Vue.config.ignoredElements = [/^ion-/, /^jeep-/];
 /* --------------------- */
 
-//Vue.use(Ionic); // commented to use the workaround
+Vue.use(Ionic); // commented to use the workaround
 /* added as a workaround */
-ionic(window);
-addIcons(ICON_PATHS);
+//ionic(window);
+//addIcons(ICON_PATHS);
 /* --------------------- */
 
 svgmorphing(window);
